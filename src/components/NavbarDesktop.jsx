8835842b39c337134ac72f820bfd19a987cf801a
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-02-15 13:07:33 -0500

    add current route styles in navbar for desktop

--- src/components/NavbarDesktop.jsx
+++ src/components/NavbarDesktop.jsx
@@ -2,6 +2,7 @@
 
 import logo from '../assets/images/logo.svg'
 import userAvatar from '../assets/images/image-avatar.png'
+import { useLocation } from 'react-router-dom'
 import React from 'react'
 import './NavbarDesktop.css'
 
@@ -11,16 +12,17 @@ const user = {
 }
 
 function NavbarMobile() {
+    const { pathname } = useLocation()
     return (
         <nav className='navbar-desktop'>
             <div className='navbar-desktop_container'>
                 <a href='/' className='navbar-logo'><img src={logo} alt="logo" /></a>
                 <ul className='navbar-nav'>
-                    <li className='navbar-nav_item'><a href="/">Collections</a></li>
-                    <li className='navbar-nav_item'><a href="/">Men</a></li>
-                    <li className='navbar-nav_item'><a href="/">Women</a></li>
-                    <li className='navbar-nav_item'><a href='/'>About</a></li>
-                    <li className='navbar-nav_item'><a href="/">Contact</a></li>
+                    <li className={`navbar-nav_item${pathname === '/collections'?' active':''}`}><a href="/">Collections</a></li>
+                    <li className={`navbar-nav_item${pathname === '/men'?' active':''}`}><a href="/">Men</a></li>
+                    <li className={`navbar-nav_item${pathname === '/'?' active':''}`}><a href="/">Women</a></li>
+                    <li className={`navbar-nav_item${pathname === '/about'?' active':''}`}><a href='/'>About</a></li>
+                    <li className={`navbar-nav_item${pathname === '/contact'?' active':''}`}><a href="/">Contact</a></li>
                 </ul>
 
             </div>
